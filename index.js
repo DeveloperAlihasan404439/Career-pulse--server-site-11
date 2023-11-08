753ed95182fa30039ed6b5e02240f6259832711c
alihasanlitonislam <alihasan404439@gmail.com>
2023-11-08 18:35:31 +0600

    server site successfull in jwt token

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cookieParser = require("cookie-parser");
+const jwt = require('jsonwebtoken')
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-// middleware
-/* app.use(cors());
-app.use(express.json());
-app.use(cookieParser()); */
 // medalwore
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json());
+app.use(cookieParser());
 // satup the mongodb in the server site
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wjgws1x.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,6 +27,20 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorizeds" });
+  }
+  jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({ message: "unauthorizeds" });
+    }
+    req.user = decoded;
+    next()
+  })
+};
+
 async function run() {
   try {
     // todo : deploy korar somay commint kore dita hove nicer code gulo
@@ -37,7 +52,22 @@ async function run() {
     // Send a ping to confirm a successful connection
     const catagoryCollection = client.db("appliction_job").collection("catagory");
     const myBidsCollection = client.db("appliction_job").collection("My_Bids");
-
+    const candidatesCollection = client.db("appliction_job").collection("candidates");
+    // jwt access token
+    app.post('/jwt', async(req, res)=>{
+      try {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.JWT_ACCESS_TOKEN, { expiresIn: '1h' })
+        res.cookie('token', token, {
+          httpOnly: true,
+          secure: false
+        }).send({success: true})
+      } catch {
+        (error) => {
+          res.status(500).send("Internal Server Error");
+        };
+      }
+    })
     // catagory collection code start
     app.get("/catagory/:type", async (req, res) => {
       try {
@@ -128,11 +158,13 @@ async function run() {
       }
     })
     // catagory collection code end
-
     // my bids collection code start
-    app.get('/myBids', async(req, res)=>{
+    app.get('/myBids', verifyToken, async(req, res)=>{
       try {
-        const result = await myBidsCollection.find().toArray()
+        if(req.query.email !== req.user.email){
+          return res.status(403).send({ message: "forbidden access" });
+        }
+        const result = await myBidsCollection.find().sort({"status": -1}).toArray()
         res.send(result)
       } catch {
         (error) => {
@@ -140,18 +172,17 @@ async function run() {
         };
       }
     })
+    // my bids status requst reject updated code 
     app.patch('/myBids/reject', async(req, res)=>{
       try {
         const id = req.query.id;
         const filter = {_id: id}
-        const body = req.body;
         const updatedMyBids ={
           $set: {
             status: 'Canceled',
           }
         }
         const result = await myBidsCollection.updateOne(filter, updatedMyBids)
-        console.log(result);
         res.send(result)
       } catch {
         (error) => {
@@ -159,6 +190,43 @@ async function run() {
         };
       }
     })
+    // my bids status requst accept updated code 
+    app.patch('/myBids/accept', async(req, res)=>{
+      try {
+        const id = req.query.id;
+        const filter = {_id: id}
+        const updatedMyBids ={
+          $set: {
+            status: 'In progress',
+          }
+        }
+        const result = await myBidsCollection.updateOne(filter, updatedMyBids)
+        res.send(result)
+      } catch {
+        (error) => {
+          res.status(500).send("Internal Server Error");
+        };
+      }
+    })
+    // my bids status requst accept updated code 
+    app.patch('/myBids/complete', async(req, res)=>{
+      try {
+        const id = req.query.id;
+        const filter = {_id: id}
+        const updatedMyBids ={
+          $set: {
+            status: 'Complete',
+          }
+        }
+        const result = await myBidsCollection.updateOne(filter, updatedMyBids)
+        res.send(result)
+      } catch {
+        (error) => {
+          res.status(500).send("Internal Server Error");
+        };
+      }
+    })
+    // my bids job added the mongodb
     app.post('/myBids', async(req, res) =>{
       try {
         const body = req.body;
@@ -171,6 +239,21 @@ async function run() {
       }
     })
     // my bids collection code end
+    app.get('/candidates', async(req, res) =>{
+      try {
+        const countNumber = req.query.pages;
+        const parPages = 6;
+        const skip = countNumber * parPages;
+        const product =  candidatesCollection.find().skip(skip).limit(parPages);
+        const result = await product.toArray()
+        const counter = await candidatesCollection.countDocuments()
+        res.send({result,counter});
+      } catch {
+        (error) => {
+          res.status(500).send("Internal Server Error");
+        };
+      }
+    })
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
